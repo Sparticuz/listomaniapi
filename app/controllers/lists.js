3598c7c62e6b554c6525b1909c5de4ced8de5316
Sparticuz <kyle@kmcnally.net>
2015-07-03 20:44:22 -0400

    basic create, get, and delete functionality

--- app/controllers/lists.js
+++ app/controllers/lists.js
@@ -6,52 +6,50 @@ var express = require('express');
 var router = express.Router();
 
 router.route('/lists')
-	.get(function(req,res,next){
+	.get(function(req,res){
 		//Retrieves all lists
 		List.find(function(err,lists){
 			if (err) return res.send(err);
 			console.log('GET /lists');
 			res.json(lists);
-		})
-		//next();
+		});
 	})
-	.post(function(req,res,next){
-		//Invalid, return error
-		console.log('POST /lists');
-		next();
-	})
-	.put(function(req,res,next){
+	.post(function(req,res){res.status(405).send('Please select a list');})
+	.put(function(req,res){
 		//Add's new list
-		console.log('PUT /lists');
-		next();
+		console.log(req.body);
+		var list = new List();
+		list = req.body;
+		list.save(function(err){
+			if (err) return res.send(err);
+			console.log('PUT /lists');
+			res.status(201).send();
+		});
 	})
-	.delete(function(req,res,next){
-		//Invalid, return error
-		console.log('DELETE /lists');
-		next();
+	.delete(function(req,res){res.status(405).send('Cannot DELETE All lists. Please select a list.');
 	});
 
 router.route('/lists/:id')
-	.get(function(req,res,next){
+	.get(function(req,res){
 		//Retrieves single list
-		console.log('GET /lists');
-		res.send('{test:test}');
-		next();
+		List.findById(req.params.id,function(err,list){
+			if (err) return res.send(err);
+			console.log('GET /lists');
+			res.json(list);
+		})
 	})
-	.post(function(req,res,next){
-		//Updates a single list
+	.post(function(req,res){
+		//TODO: Updates a single list
 		console.log('POST /lists');
-		next();
 	})
-	.put(function(req,res,next){
-		//Invalid, return error
-		console.log('PUT /lists');
-		next();
-	})
-	.delete(function(req,res,next){
+	.put(function(req,res){res.status(405).send('Cannot PUT a new list onto an existing list, Please use PUT /lists');})
+	.delete(function(req,res){
 		//Deletes single list
-		console.log('DELETE /lists');
-		next();
+		List.findByIdAndRemove(req.body.id,function(err){
+			if (err) return res.send(err);
+			console.log('DELETE /lists');
+			res.status(201).send();
+		});
 	});
 
 module.exports = router;
