c5e568512f5d2524e5f661a224a77432f9181e21
Kyle McNally <kyle@kmcnally.net>
2015-10-02 13:17:46 -0400

    Cleanup auth js file

--- app/controllers/authenticate.js
+++ app/controllers/authenticate.js
@@ -45,42 +45,6 @@ passport.use(new GoogleStrategy({
     });
   }));
 
-/*var FacebookStrategy = require('passport-facebook').Strategy;
-passport.use(new FacebookStrategy({
-    clientID: config.facebook.appID,
-    clientSecret: config.facebook.appSecret,
-    callbackURL: config.facebook.callbackURL
-  },
-  function(accessToken, refreshToken, profile, done) {
-    User.findOne({
-  		'google.id': profile.id
-  	}, function (err, user) {
-  		if (err) { return done(err); }
-  		//No user found!, create one!
-  		console.log(profile);
-  		if (!user) {
-  			user = new User({
-  				user: profile.username || profile.displayName,
-				google: {
-    				id : profile.id,
-        			token : accessToken,
-        			displayName : profile.displayName,
-        			username : profile.username
-    			},
-    			avatar: profile.photos[0].value,
-  			});
-  			user.save(function(err){
- 				if (err) console.log(err);
-                return done(err, user);
-  			});
-  		} else {
-  			//User found!
-  			return done(err, user);
-  		}
-    });
-  }
-));*/
-
 passport.serializeUser(function(user, done) {
   done(null, user);
 });
@@ -109,32 +73,6 @@ router.route('/authenticate/google/callback')
 			},config.secret,{
 				expiresInMinutes: 1440
 			}));
-		/*res.status(200).json({
-			success: true,
-			token: jwt.sign({
-				_id: req.user._id,
-				user: req.user.user,
-				admin: req.user.admin
-			},config.secret,{
-				expiresInMinutes: 1440
-			})
-		});*/
 	});
 
-/*router.route('/authenticate/facebook')
-	.get(passport.authenticate('facebook', {
-		session:false
-	}),function(req,res){
-		// The request will be redirected to Google for authentication, so this
-    	// function will not be called.
-	});
-
-router.route('/authenticate/facebook/callback')
-	.get(passport.authenticate('facebook', {
-		failureRedirect:'/login'
-	}), function(req,res){
-		console.log(req);
-		res.status(200).send();
-	});
-*/
 module.exports = router;
