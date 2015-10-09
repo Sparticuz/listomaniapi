74b6c5e312dafc2e43ca66d6be8dfa73a2ee90bb
Kyle McNally <kyle@kmcnally.net>
2015-10-09 15:12:06 -0400

    another typo
    doh!

--- app/server.js
+++ app/server.js
@@ -60,7 +60,7 @@ app.use('/api', publicLists);
 app.use('/api', function(req,res,next){
     var token = req.body.token || req.query.token || req.headers['x-access-token'];
     if (token) {
-        jwt.verify(token,config.secret,function(err,decoded){
+        jwt.verify(token,config.jwt_secret,function(err,decoded){
             if (err) {
                 return res.status(401).json({
                     success: false,
